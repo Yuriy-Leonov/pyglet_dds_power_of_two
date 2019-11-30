2fd5bfde08e86f102113b16fa60b3d6b8e92252b
Yuriy-Leonov <tavoseg@gmail.com>
2019-11-30 15:20:45 +0200

    add example for rotation textures

--- performance_tests/sprite_count_test.py
+++ performance_tests/sprite_count_test.py
@@ -26,6 +26,82 @@ class Window(pyglet.window.Window):
     def update(self, delta_time):
         pass
 
+    def get_tex_map(self, tex_coords):
+        tex_map = tex_coords
+        # mirror
+        # 1 2   ->   2 1
+        # 3 4   ->   4 3
+        # tex_map = [
+        #     tex_map[6], tex_map[1], tex_map[2],
+        #     tex_map[9], tex_map[4], tex_map[5],
+        #     tex_map[0], tex_map[7], tex_map[8],
+        #     tex_map[3], tex_map[10], tex_map[11],
+        # ]
+
+        # diagonal mirror
+        # 1 2   ->   1 3
+        # 3 4   ->   2 4
+        # tex_map = [
+        #     tex_map[3], tex_map[10], tex_map[11],
+        #     tex_map[6], tex_map[1], tex_map[2],
+        #     tex_map[9], tex_map[4], tex_map[5],
+        #     tex_map[0], tex_map[7], tex_map[8],
+        # ]
+
+        # horizontal mirror
+        # 1 2   ->   1 4
+        # 3 4   ->   3 2
+        # tex_map = [
+        #     tex_map[0], tex_map[7], tex_map[8],
+        #     tex_map[3], tex_map[10], tex_map[11],
+        #     tex_map[6], tex_map[1], tex_map[2],
+        #     tex_map[9], tex_map[4], tex_map[5],
+        # ]
+
+        # diagonal mirror 2
+        # 1 2   ->   4 2
+        # 3 4   ->   3 1
+        # tex_map = [
+        #     tex_map[9], tex_map[4], tex_map[5],
+        #     tex_map[0], tex_map[7], tex_map[8],
+        #     tex_map[3], tex_map[10], tex_map[11],
+        #     tex_map[6], tex_map[1], tex_map[2],
+        # ]
+
+        # counterclockwise rotation 90* (- 90*)
+        # 1 2   ->   2 4
+        # 3 4   ->   1 3
+        # tex_map = [
+        #     tex_map[9], tex_map[10], tex_map[11],
+        #     tex_map[0], tex_map[1], tex_map[2],
+        #     tex_map[3], tex_map[4], tex_map[5],
+        #     tex_map[6], tex_map[7], tex_map[8],
+        #
+        # ]
+
+        # rotation 180*
+        # 1 2   ->   4 3
+        # 3 4   ->   2 1
+        # tex_map = [
+        #     tex_map[6], tex_map[7], tex_map[8],
+        #     tex_map[9], tex_map[10], tex_map[11],
+        #     tex_map[0], tex_map[1], tex_map[2],
+        #     tex_map[3], tex_map[4], tex_map[5],
+        #
+        # ]
+
+        # + 90*
+        # 1 2   ->   3 1
+        # 3 4   ->   4 2
+        # tex_map = [
+        #     tex_map[3], tex_map[4], tex_map[5],
+        #     tex_map[6], tex_map[7], tex_map[8],
+        #     tex_map[9], tex_map[10], tex_map[11],
+        #     tex_map[0], tex_map[1], tex_map[2],
+        #
+        # ]
+        return tex_map
+
     def create_sprite(self, x, y):
         x = x * 32
         y = y * 32 + 20
